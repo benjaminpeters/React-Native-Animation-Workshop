755c1575f995d0f1a7108edb773b16582e466419
benjaminpeters <benpeters49@gmail.com>
2022-08-04 17:05:23 -0400

    Playing around with some basic reanimated styling

--- Home/HomeScreen.tsx
+++ Home/HomeScreen.tsx
@@ -4,5 +4,13 @@ import {ScrollView} from 'react-native';
 import {HomeScreenButton} from './HomeScreenButton';
 
 export const HomeScreen = () => {
-  return <ScrollView></ScrollView>;
+  return (
+    <ScrollView>
+      <HomeScreenButton
+        title="Basic"
+        description="Some simple rotation and animations"
+        route="Basics"
+      />
+    </ScrollView>
+  );
 };
--- NavigationTree.tsx
+++ NavigationTree.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {HomeScreen} from './Home';
+import {Basics} from './Screens/Basics';
 
 const linking = {
   config: {
     screens: {
       Home: '',
+      Basics: 'basics',
     },
   },
   prefixes: ['rnskia://'],
@@ -23,6 +25,14 @@ const NavigationTree = () => {
           component={HomeScreen}
           options={{
             title: '🎨 Hello',
+            headerTitleAlign: 'center',
+          }}
+        />
+        <Stack.Screen
+          name="Basics"
+          component={Basics}
+          options={{
+            title: 'Basics',
             headerTitleAlign: 'center',
           }}
         />
